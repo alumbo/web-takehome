c6540b423d38846baa50ae8cb6519c3e3db8133a
Matthieu Chavigny <matthieu.chavigny@relyfe.com>
2022-02-18 15:43:26 +0100

    layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,32 @@
-import generateUsers from 'data/users';
-import generateChats from 'data/chats';
+import generateUsers from "data/users";
+import generateChats from "data/chats";
 
-import ClubLogoSvg from 'icons/club-logo.svg';
-import UserInterface from 'types/users';
-import ChatInterface from 'types/chats';
-import MessageInterface from 'types/messages';
+import ClubLogoSvg from "icons/club-logo.svg";
+import UserInterface from "types/users";
+import ChatInterface from "types/chats";
+import MessageInterface from "types/messages";
+import NavHeader from "components/NavHeader";
 
 interface LandingPageProps {
   users: UserInterface[];
   chats: ChatInterface[];
   messages: MessageInterface[];
-};
+}
 
-const LandingPage = ({ users, chats, messages}: LandingPageProps) => {
-  console.log({ users, chats, messages });
-
+const LandingPage = ({ users, chats, messages }: LandingPageProps) => {
+  console.log({ users, chats, messages });
   return (
     <>
-      <div className="bg-black h-screen w-full flex justify-center items-center flex-col">
-        <ClubLogoSvg className="w-[100px]" />
-        <p className="text-white text-[23px] mt-[8px]">
-          takehome
-        </p>
-      </div>
+      <section className="flex w-full h-[100vh]">
+        <nav className="w-[268px] p-[10px]">
+          <ClubLogoSvg className="w-[114px] m-auto mt-[10px] mb-[10px]" />
+          <NavHeader chatsCount={chats.length} />
+        </nav>
+        <article className="flex-1">
+          <p className="text-[23px] mt-[8px]">main chat</p>
+        </article>
+        <aside className="w-[268px]">search</aside>
+      </section>
     </>
   );
 };
@@ -31,13 +35,18 @@ export const getServerSideProps = () => {
   // This is only an exemple of how you could pass data from server to client,
   // you may create another page and not use that use
   const users: UserInterface[] = generateUsers();
-  const { chats, messages }: { chats: ChatInterface[], messages: MessageInterface[] } = generateChats();
+  const {
+    chats,
+    messages,
+  }: { chats: ChatInterface[]; messages: MessageInterface[] } = generateChats();
 
   return {
     props: {
-      users, chats, messages,
+      users,
+      chats,
+      messages,
     },
   };
-}
+};
 
 export default LandingPage;
---
+++ src/components/NavHeader.tsx
@@ -0,0 +1,15 @@
+import ProfilePicture from "images/profilePicture.png";
+
+interface NavHeaderProps {
+  chatsCount: number;
+}
+const NavHeader = ({ chatsCount }: NavHeaderProps) => {
+  return (
+    <div className="flex justify-between items-center p-[5px] mt-[10px]">
+      <img width={35} src={ProfilePicture} />
+      <span className="text-sm">{chatsCount} chats</span>
+    </div>
+  );
+};
+
+export default NavHeader;
