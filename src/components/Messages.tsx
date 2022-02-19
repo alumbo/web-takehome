2552ed49f948865c23248caf9381c86aac4b1fa9
Matthieu Chavigny <matthieu.chavigny@relyfe.com>
2022-02-19 08:18:14 +0100

    scroll messages to bottom

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -3,19 +3,33 @@ import UserInterface from 'types/users';
 import Avatar from './Avatar';
 import ProfilePicture from 'images/profilePicture.png';
 import moment from 'moment';
+import { useEffect, useRef } from 'react';
 
 interface MessagesProps {
   user: UserInterface;
   messages: MessageInterface[];
 }
 const Messages = ({ user, messages }: MessagesProps) => {
+  const headerHeight = 75;
+  const messagesView = useRef<HTMLDivElement>();
+  useEffect(() => {
+    messagesView.current.scrollTop = messagesView.current.scrollHeight;
+  }, [messagesView.current, user.id]);
   return (
-    <article className="flex-1 border-r border-l border-silver justify-between flex flex-col h-[100vh] overflow-auto">
-      <header className="flex items-center border-b-2 p-4">
+    <article className="flex-1 border-r border-l border-silver justify-between flex flex-col ">
+      <header
+        className={`flex items-center border-b-2 p-3 h-[${headerHeight}px]`}
+      >
         <Avatar user={user} />
         <span className="pl-3 text-xl">{user.name}</span>
       </header>
-      <div>
+      <div
+        className="overflow-auto"
+        style={{
+          height: `calc(100vh-${headerHeight}px)`
+        }}
+        ref={messagesView}
+      >
         <ul>
           {messages.map((message) => {
             return (
